3c94742b4f1d63df866cdba8701fd195805b0879
anonymous <anonymous>
2021-02-10 06:34:52 +0100

    SURAPP-177: Authorize users and show how to get the information from the user

---
+++ pages/me.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="mb-3 text-2xl">{{ user.name }}</h1>
+    <p>{{ user.email }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  middleware: 'auth',
+})
+export default class SearchPage extends Vue {
+  get user() {
+    return this.$auth.user
+  }
+}
+</script>
