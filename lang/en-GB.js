5cd024fd5d717586b8987ef8d0ee6409082f1900
anonymous <anonymous>
2021-02-11 14:56:00 +0100

    Added translations

--- lang/en-GB.js
+++ lang/en-GB.js
@@ -4,11 +4,32 @@ export default {
 
   pages: {
     index: {
-      title: '',
-      subtitle: '',
+      title: 'Together we create impact!',
+      subtitle: 'Knowledge from higher education in the Netherlands',
       search_blocks: {
-        title: '',
-        items: [],
+        title: 'I\'m looking for',
+        items: [
+          {
+            title: 'Projects',
+            description:
+              'Research projects and programs with underlying products',
+          },
+          {
+            title: 'Results',
+            description:
+              'Outcomes and results of practice-oriented studies',
+          },
+          {
+            title: 'People',
+            description:
+              'People involved in practice-oriented research',
+          },
+          {
+            title: 'Parties',
+            description:
+              'Organizations and institutions involved in practice-oriented research',
+          },
+        ],
       },
     },
     search: {
