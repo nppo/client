b5a6b461f62c0bd8e403b18ae9b705d84eacd66c
anonymous <anonymous>
2021-02-12 03:18:12 +0100

    SURAPP-171: Removes linting suppression comment

--- pages/index.vue
+++ pages/index.vue
@@ -74,7 +74,6 @@ export default class IndexPage extends Vue {
     'pages.index.search_blocks.items'
   )
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setFilters(type: string, filters: Array<any>) {
     this.activeFilters = filters
     this.$accessor.search.setFilter({ type, values: filters })
