cd4e3580fdfd810d59ca0202aef1a3f3c4138f37
anonymous <anonymous>
2021-03-02 11:38:55 +0100

    SURAPP-225: SURAPP-218: Add linked people to project page

---
+++ pages/project/_id/people.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="flex items-center justify-between mb-5">
+      <h2 class="mb-2 text-3xl">
+        {{ $t('types.person') }}
+      </h2>
+    </div>
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div v-for="person in projectContent" :key="person.id">
+        <PersonBlock :person="person" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { Person } from '~/types/entities'
+
+@Component
+export default class ProjectPeoplePage extends Vue {
+  @Prop({ type: Array, required: true }) projectContent!: Person
+}
+</script>
