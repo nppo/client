5bbdbaffe23d201629127ea52ec20ff8e3947d53
anonymous <anonymous>
2021-03-02 12:27:45 +0100

    SURAPP-221: Hide pagination when lower than slides to show

--- pages/person/_id.vue
+++ pages/person/_id.vue
@@ -26,7 +26,7 @@
               <Badge :text="`${person.projects.length}`" color="yellow-brand" />
             </div>
 
-            <div class="flex space-x-1">
+            <div class="flex space-x-1" v-if="person.projects.length > 3">
               <div
                 class="flex justify-center w-8 h-8 p-2 text-blue-800 rounded-full cursor-pointer bg-yellow-brand"
                 @click="$refs.projectSlider.previous()"
