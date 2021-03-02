d57ca6390893a22cf39f1f4fa1ae32ea8e04b54b
anonymous <anonymous>
2021-03-02 12:01:12 +0100

    SURAPP-222: Show linked parties on person page

--- pages/person/_id.vue
+++ pages/person/_id.vue
@@ -85,6 +85,40 @@
               <ProductBlock :product="product" />
             </div>
           </BlockSlider>
+
+          <div class="flex items-center justify-between mt-10 mb-6">
+            <div class="flex items-center space-x-5">
+              <h2 class="text-4xl font-bold">
+                {{ $t('entities.party.plural') }}
+              </h2>
+              <Badge :text="`${person.parties.length}`" color="yellow-brand" />
+            </div>
+
+            <div class="flex space-x-1">
+              <div
+                class="flex justify-center w-8 h-8 p-2 text-blue-800 rounded-full cursor-pointer bg-yellow-brand"
+                @click="$refs.partySlider.previous()"
+              >
+                <font-awesome-icon class="text-base" icon="arrow-left" />
+              </div>
+              <div
+                class="flex justify-center w-8 h-8 p-2 text-blue-800 rounded-full cursor-pointer bg-yellow-brand"
+                @click="$refs.partySlider.next()"
+              >
+                <font-awesome-icon class="text-base" icon="arrow-right" />
+              </div>
+            </div>
+          </div>
+
+          <BlockSlider ref="partySlider" :slides-to-show="3">
+            <div
+              v-for="party in person.parties"
+              :key="party.id"
+              class="h-full px-2"
+            >
+              <PartyBlock :party="party" />
+            </div>
+          </BlockSlider>
         </div>
       </div>
     </div>
