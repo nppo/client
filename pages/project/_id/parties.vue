cd1ca1454b591e3aa02dadfa0e08f058e7759386
anonymous <anonymous>
2021-03-02 11:39:12 +0100

    SURAPP-225: SURAPP-226: Add linked parties to project page

---
+++ pages/project/_id/parties.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="flex items-center justify-between mb-5">
+      <h2 class="mb-2 text-3xl">
+        {{ $t('types.party') }}
+      </h2>
+    </div>
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div v-for="party in projectContent" :key="party.id">
+        <PartyBlock :party="party" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { Party } from '~/types/entities'
+
+@Component
+export default class ProjectPartyPage extends Vue {
+  @Prop({ type: Array, required: true }) projectContent!: Party
+}
+</script>
