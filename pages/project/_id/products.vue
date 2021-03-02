c5c0c6c304632ebb3153b2d85dbb1c99525200fd
anonymous <anonymous>
2021-03-02 11:38:10 +0100

    SURAPP-225: SURAPP-220: Add linked products to project page

--- pages/project/_id/products.vue
+++ pages/project/_id/products.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="container relative h-full px-16 mx-auto">
-    <div class="grid grid-cols-4 gap-4 mb-2">
-      <div class="col-span-3 mr-10">
-        <div v-for="product in project" :key="product.id">
-          {{ product.title }}
-        </div>
+  <div>
+    <div class="flex items-center justify-between mb-5">
+      <h2 class="mb-2 text-3xl">
+        {{ $t('types.product') }}
+      </h2>
+    </div>
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div v-for="product in projectContent" :key="product.id">
+        <ProductBlock :product="product" />
       </div>
-      <div />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, mixins } from 'nuxt-property-decorator'
-import NavigationRouterHook from '~/mixins/navigation-router-hook'
-import { Project } from '~/types/entities'
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { Product } from '~/types/entities'
 
 @Component
-export default class ProjectProductsPage extends mixins(NavigationRouterHook) {
-  @Prop({ type: Array, required: true }) project!: Project
+export default class ProjectProductsPage extends Vue {
+  @Prop({ type: Array, required: true }) projectContent!: Product
 }
 </script>
