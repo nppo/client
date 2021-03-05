a2062ab9991f604951b07a60f7812dcfec4cd995
anonymous <anonymous>
2021-03-05 13:38:33 +0100

    SURAPP-235: Change vue-gates plugin file to TS

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ export default {
     '~/plugins/repositories.ts',
     '~/plugins/mock-axios.ts',
     '~/plugins/axios.ts',
-    '~/plugins/vue-gates.js',
+    '~/plugins/vue-gates.ts',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
