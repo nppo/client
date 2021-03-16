fcd96855a54e4f65e418c7c3ff21a06885039fc5
anonymous <anonymous>
2021-03-16 11:32:52 +0100

    SURAPP-258: Make the product page into an actual page

--- pages/product/create.vue
+++ pages/product/create.vue
@@ -1,93 +1,103 @@
 <template>
-  <div class="mt-18">
-    <h1 class="mb-6 text-4xl font-bold">
-      {{ $t('pages.product.create.headings.create') }}
-    </h1>
+  <div class="flex-1 pb-24">
+    <Header has-dark-header has-search-bar :has-image="false" />
 
-    <ValidationObserver>
-      <form
-        class="p-4 overflow-hidden bg-white rounded-md shadow"
-        @submit.prevent="create"
-      >
-        <div class="flex justify-between mb-6 space-x-32">
-          <div class="w-6/12">
-            <SelectInput
-              :value.sync="formData.type"
-              :name="$t('pages.product.create.form.labels.type')"
-              :label="$t('pages.product.create.form.labels.type')"
-              :options="types"
-              :on-selected="(option) => option.label"
-            />
+    <div class="container mx-auto mt-6">
+      <div class="flex items-center justify-between">
+        <BackButton :has-navigated-internal="hasNavigatedInternal" />
+      </div>
 
-            <TextInput
-              :value.sync="formData.title"
-              :name="$t('pages.product.create.form.labels.title')"
-              :label="$t('pages.product.create.form.labels.title')"
-              :error-message="$t('validation.required')"
-              :has-errors.sync="titleError"
-            />
+      <div class="mt-18">
+        <h1 class="mb-6 text-4xl font-bold">
+          {{ $t('pages.product.create.headings.create') }}
+        </h1>
 
-            <div class="flex flex-col mb-4">
-              <label
-                class="pl-3 mb-1"
-                :for="$t('pages.product.create.form.labels.description')"
-              >
-                {{ $t('pages.product.create.form.labels.description') }}
-              </label>
-              <textarea
-                :id="$t('pages.product.create.form.labels.description')"
-                v-model="formData.description"
-                rows="6"
-                class="p-3 font-bold rounded-md shadow focus:outline-none"
-              />
+        <ValidationObserver>
+          <form
+            class="p-4 overflow-hidden bg-white rounded-md shadow"
+            @submit.prevent="create"
+          >
+            <div class="flex justify-between mb-6 space-x-32">
+              <div class="w-6/12">
+                <SelectInput
+                  :value.sync="formData.type"
+                  :name="$t('pages.product.create.form.labels.type')"
+                  :label="$t('pages.product.create.form.labels.type')"
+                  :options="types"
+                  :on-selected="(option) => option.label"
+                />
+
+                <TextInput
+                  :value.sync="formData.title"
+                  :name="$t('pages.product.create.form.labels.title')"
+                  :label="$t('pages.product.create.form.labels.title')"
+                  :error-message="$t('validation.required')"
+                  :has-errors.sync="titleError"
+                />
+
+                <div class="flex flex-col mb-4">
+                  <label
+                    class="pl-3 mb-1"
+                    :for="$t('pages.product.create.form.labels.description')"
+                  >
+                    {{ $t('pages.product.create.form.labels.description') }}
+                  </label>
+                  <textarea
+                    :id="$t('pages.product.create.form.labels.description')"
+                    v-model="formData.description"
+                    rows="6"
+                    class="p-3 font-bold rounded-md shadow focus:outline-none"
+                  />
+                </div>
+              </div>
+
+              <div class="w-6/12">
+                <div class="flex flex-col mb-4">
+                  <FileInput
+                    :value.sync="formData.file"
+                    :name="$t('pages.product.create.form.labels.file')"
+                    :label="$t('pages.product.create.form.labels.file')"
+                  />
+                  <Multiselect
+                    :entity.sync="formData.tags"
+                    :options="tags"
+                    :label="$t('pages.product.create.form.labels.tags')"
+                  />
+
+                  <Multiselect
+                    :entity.sync="formData.themes"
+                    :options="themes"
+                    :label="$t('pages.product.create.form.labels.themes')"
+                  />
+
+                  <Multiselect
+                    :entity.sync="formData.people"
+                    :options="people"
+                    :label="$t('pages.product.create.form.labels.people')"
+                    :option-label="
+                      (option) => `${option.firstName} ${option.lastName}`
+                    "
+                  />
+                  <Multiselect
+                    :entity.sync="formData.parties"
+                    :options="parties"
+                    :label="$t('pages.product.create.form.labels.parties')"
+                    :option-label="(option) => `${option.name}`"
+                  />
+                </div>
+              </div>
             </div>
-          </div>
 
-          <div class="w-6/12">
-            <div class="flex flex-col mb-4">
-              <FileInput
-                :value.sync="formData.file"
-                :name="$t('pages.product.create.form.labels.file')"
-                :label="$t('pages.product.create.form.labels.file')"
-              />
-              <Multiselect
-                :entity.sync="formData.tags"
-                :options="tags"
-                :label="$t('pages.product.create.form.labels.tags')"
-              />
-
-              <Multiselect
-                :entity.sync="formData.themes"
-                :options="themes"
-                :label="$t('pages.product.create.form.labels.themes')"
-              />
-
-              <Multiselect
-                :entity.sync="formData.people"
-                :options="people"
-                :label="$t('pages.product.create.form.labels.people')"
-                :option-label="
-                  (option) => `${option.firstName} ${option.lastName}`
-                "
-              />
-              <Multiselect
-                :entity.sync="formData.parties"
-                :options="parties"
-                :label="$t('pages.product.create.form.labels.parties')"
-                :option-label="(option) => `${option.name}`"
-              />
-            </div>
-          </div>
-        </div>
-
-        <button
-          class="self-start px-4 py-2 text-sm text-white rounded bg-orange-brand"
-          type="submit"
-        >
-          {{ $t('general.actions.create') }}
-        </button>
-      </form>
-    </ValidationObserver>
+            <button
+              class="self-start px-4 py-2 text-sm text-white rounded bg-orange-brand"
+              type="submit"
+            >
+              {{ $t('general.actions.create') }}
+            </button>
+          </form>
+        </ValidationObserver>
+      </div>
+    </div>
   </div>
 </template>
 
