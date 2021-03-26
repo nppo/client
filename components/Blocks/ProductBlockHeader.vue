7820c1bedfd2ab62e5efbbf78acb2a7ba3078ae0
anonymous <anonymous>
2021-03-26 12:19:55 +0100

    SURAPP-279: Remove onmouseenter

--- components/Blocks/ProductBlockHeader.vue
+++ components/Blocks/ProductBlockHeader.vue
@@ -14,7 +14,6 @@
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen
-      :onmouseenter="playVideo()"
     >
     </iframe>
 
